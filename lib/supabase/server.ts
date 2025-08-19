1d12e7dfcfea4cf5f5e98b7186adc5322f61a35e
u-hossy <95125392+u-hossy@users.noreply.github.com>
2025-08-19 13:57:52 +0900

    fix environment variable name

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -11,7 +11,7 @@ export async function createClient() {
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
@@ -20,7 +20,7 @@ export async function createClient() {
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options),
+              cookieStore.set(name, value, options)
             );
           } catch {
             // The `setAll` method was called from a Server Component.
@@ -29,6 +29,6 @@ export async function createClient() {
           }
         },
       },
-    },
+    }
   );
 }
