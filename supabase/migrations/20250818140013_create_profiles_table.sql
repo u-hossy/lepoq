e8d1bf2816a75d07ab44a5ad24522b9837b0ac1c
u-hossy <95125392+u-hossy@users.noreply.github.com>
2025-08-20 10:49:30 +0900

    format SQL

--- supabase/migrations/20250818140013_create_profiles_table.sql
+++ supabase/migrations/20250818140013_create_profiles_table.sql
@@ -1,21 +1,32 @@
-create table profiles (
+create table
+  profiles (
     id uuid references auth.users not null primary key,
-    created_at timestamp not null default now(),
-    updated_at timestamp not null default now(),
+    created_at timestamp not null default now (),
+    updated_at timestamp not null default now (),
     display_id text unique,
     username text,
-
     constraint display_id_length check (char_length(display_id) >= 3)
+  );
+
+alter table profiles enable row level security;
+
+create policy "Public profiles are viewable by everyone." on profiles for
+select
+  using (true);
+
+create policy "Users can insert their own profile." on profiles for insert
+with
+  check (
+    (
+      select
+        auth.uid ()
+    ) = id
+  );
+
+create policy "Users can update own profile." on profiles for
+update using (
+  (
+    select
+      auth.uid ()
+  ) = id
 );
-
-alter table profiles
-  enable row level security;
-
-create policy "Public profiles are viewable by everyone." on profiles
-  for select using (true);
-
-create policy "Users can insert their own profile." on profiles
-  for insert with check ((select auth.uid()) = id);
-
-create policy "Users can update own profile." on profiles
-  for update using ((select auth.uid()) = id);
--- supabase/migrations/20250818140018_create_posts_table.sql
+++ supabase/migrations/20250818140018_create_posts_table.sql
@@ -1,13 +1,15 @@
-create table posts (
-    id uuid primary key default gen_random_uuid(),
-    created_at timestamp not null default now(),
-    updated_at timestamp not null default now(),
-    user_id uuid not null references auth.users(id),
-    question text not null,
-    hint text,
-    answer text not null,
-    tags text[] not null default '{}',
-    is_draft boolean not null default true,
-    likes_count integer not null default 0,
-    comments_count integer not null default 0
-);
+create table
+    posts (
+        id uuid primary key default gen_random_uuid (),
+        created_at timestamp not null default now (),
+        updated_at timestamp not null default now (),
+        user_id uuid not null,
+        foreign key (user_id) references auth.users (id),
+        question text not null,
+        hint text,
+        answer text not null,
+        tags text[] not null default '{}',
+        is_draft boolean not null default true,
+        likes_count integer not null default 0,
+        comments_count integer not null default 0
+    );
